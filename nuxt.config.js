7f170414cc850f4b9a538e242ad55d031b5046a1
havvtom <havvtom@gmail.com>
2021-01-06 17:35:15 +0200

    added new API_URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'https://havvjobs-api.herokuapp.com/'
+    baseURL: 'https://havvjobs-api.herokuapp.com/api'
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
